8a530a203d74bf509df808aecb318428c0ea23d9
KingMario <gcyyq@hotmail.com>
2025-02-17 17:56:58 +0800

    refactor MiddleButton spoon to improve middle mouse button simulation logic

--- Spoons/MiddleButton.spoon/init.lua
+++ Spoons/MiddleButton.spoon/init.lua
@@ -8,32 +8,39 @@ obj.author = "Mario Studio<gcyyq@hotmail.com>"
 obj.homepage = "https://github.com/KingMario/spoons"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
+-- Track if middle button down was triggered by this spoon
+local middleButtonDownTriggered = false
+
 -- Function to start the spoon
 function obj:start()
-    self.eventtap = hs.eventtap.new({hs.eventtap.event.types.leftMouseDown, hs.eventtap.event.types.leftMouseUp},
+    self.eventtap = hs.eventtap.new(
+        {hs.eventtap.event.types.leftMouseDown, hs.eventtap.event.types.leftMouseUp},
         function(event)
             local flags = event:getFlags()
             local button = event:getProperty(hs.eventtap.event.properties.mouseEventButtonNumber)
+            local mousePosition = hs.mouse.absolutePosition()
+            local currentEvent = event:getType()
+            local middleButtonEventType
 
-            if flags.fn and button == 0 then -- Check if fn key is held and left mouse button is involved
-                local middleButtonEventType = event:getType() == hs.eventtap.event.types.leftMouseDown and
-                                                  hs.eventtap.event.types.otherMouseDown or
-                                                  hs.eventtap.event.types.otherMouseUp
-                local mousePosition = hs.mouse.absolutePosition()
-                -- print("Mouse Position:", mousePosition)
-
-                if middleButtonEventType and mousePosition then
-                    -- Create a new mouse event with button number 2
-                    local middleButtonEvent = hs.eventtap.event.newMouseEvent(middleButtonEventType, mousePosition)
-                    middleButtonEvent:setProperty(hs.eventtap.event.properties.mouseEventButtonNumber, 2)
-                    if middleButtonEvent then
-                        middleButtonEvent:post()
-                        return true -- Consume the event
-                    end
-                end
+            if flags.fn and button == 0 and currentEvent == hs.eventtap.event.types.leftMouseDown then
+                middleButtonEventType = hs.eventtap.event.types.otherMouseDown
+            elseif middleButtonDownTriggered and button == 0 and currentEvent == hs.eventtap.event.types.leftMouseUp then
+                middleButtonEventType = hs.eventtap.event.types.otherMouseUp
             end
+
+            if middleButtonEventType and mousePosition then
+                -- Create a new mouse event with button number 2
+                local middleButtonEvent = hs.eventtap.event.newMouseEvent(middleButtonEventType, mousePosition)
+                middleButtonEvent:setProperty(hs.eventtap.event.properties.mouseEventButtonNumber, 2)
+                middleButtonDownTriggered = middleButtonEventType == hs.eventtap.event.types.otherMouseDown
+                middleButtonEvent:post()
+
+                return true -- Consume the event
+            end
+
             return false -- Do not consume the event
-        end)
+        end
+    )
     self.eventtap:start()
 end
 
